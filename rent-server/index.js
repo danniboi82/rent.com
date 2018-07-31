dfc2f5758c71ee0a3526328c10b2ff290e283391
Dan Oh <danoh82@gmail.com>
2018-07-30 19:58:28 -0700

    adding basic error handlers

--- rent-server/index.js
+++ rent-server/index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const errorHandler = require('./handlers/error');
 
 const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+
+app.use(function (req, res, next) {
+    let err = new Error("not found");
+    err.status = 404;
+    next(err);
+})
+
+app.use(errorHandler);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
