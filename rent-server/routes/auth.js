5419ba55ca6526bbc02edc5af84329a3bb6cd560
Dan Oh <danoh82@gmail.com>
2018-07-30 20:52:07 -0700

    removing .env

--- rent-server/routes/auth.js
+++ rent-server/routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { signup, signin } = require('../handlers/auth');
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 module.exports = router;
