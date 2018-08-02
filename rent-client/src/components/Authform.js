c17fc6036922d882657779cc544bdf354a9a51d2
Dan Oh <danoh82@gmail.com>
2018-08-01 19:50:52 -0700

    adding props and form for signup

--- rent-client/src/components/Authform.js
+++ rent-client/src/components/Authform.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Authform = props => {
-    return (
-        <div>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" name='username' />
-                    <label htmlFor="Name">Name:</label>
-                    <input type="text" name='username' />
+class Authform extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            username: '',
+            password: '',
+            profileImageUrl: ''
+        }
+    }
+    render() {
+        const {email, username, password, profileImageUrl} = this.state;
+        const {buttonText, heading} = this.props;
+        return (
+            <div className='row justify-content-md-center text-center'>
+                <div className="col-md-6">
+                    <form onSubmit={this.handleSubmit}>
+                        <h2>{heading}</h2>
+                        <div className="form-group">
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                type="text"
+                                className='form-control'
+                                id='email'
+                                onChange={this.handleChange}
+                                value='email'
+                                name='email'/>
+                            <label htmlFor="password">Password:</label>
+                            <input
+                                type="password"
+                                className='form-control'
+                                id='password'
+                                onChange={this.handleChange}
+                                name='password'/>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default Authform;
